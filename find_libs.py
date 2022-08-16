0f2fceeb4740b80ba51c4d708dc4e9aa453b3554
vsoch <vsoch@users.noreply.github.com>
2022-08-16 16:37:40 -0600

    programatically find .so libs
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ find_libs.py
@@ -0,0 +1,27 @@
+# Find any .so libraries in a dest and move to a source.
+
+import os
+import sys
+import fnmatch
+import shutil
+
+def recursive_find(base, pattern="*.so"):
+    for root, _, filenames in os.walk(base):
+        for filename in fnmatch.filter(filenames, pattern):
+            yield os.path.join(root, filename)
+
+
+def main(src, dest):
+    for lib in recursive_find(src, "*.so"):
+        lib_dir = os.path.dirname(src).replace(src, "").strip("/")
+        dest_lib = os.path.join(dest, lib_dir, os.path.basename(lib))
+        print("Copying %s to %s" % (lib, dest_lib))
+        shutil.copyfile(lib, dest_lib)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("Expecting a src and dest as two arguments.")
+    src = os.path.abspath(sys.argv[1])
+    dest = os.path.abspath(sys.argv[2])
+    main(src, dest)
