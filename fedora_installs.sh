51b69ea7ff1b99ccc3b6cd59ab8e1f34ea9b211d
vsoch <vsoch@users.noreply.github.com>
2022-08-16 15:22:37 -0600

    remove "libicu67"
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- fedora_installs.sh
+++ fedora_installs.sh
@@ -4,8 +4,10 @@ declare -a libs=("libadwaita-qt5" "aspell" "boost-log", "clucene-core" "libdap" 
   "dovecot" "exiv2-libs" "gdal-libs" "geos" "glibmm24" "mozilla-openh264" "hdf5" "libicu" "dyninst" "webkit2gtk3-jsc"
   "webkit2gtk3-jsc" "libjxl" "libkml" "libmusicbrainz5" "openexr-libs" "openh264" "mesa-libOSMesa" "proj"
   "qt5-qtwayland" "qt5-qtxmlpatterns" "SDL2_image" "libstdc++" "taglib" "libreoffice-ure" "vtk"
-  "webrtc-audio-processing" "libicu67"
+  "webrtc-audio-processing"
 )
+
+# failed could not find "libicu67" (removed)
 
 dnf install -y findutils
 for lib in "${libs[@]}"; do
