eb0d22e3e018436846119ce04a25fb70d44762c4
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-08-18 12:04:23 -0600

    start of work to test fedora lib comparison (#2)
    * start of work to test fedora lib comparison
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ fedora_installs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -a libs=("libadwaita-qt5" "aspell" "boost-log", "clucene-core" "libdap" "samba-libs" "djvulibre-libs"
+  "dovecot" "exiv2-libs" "gdal-libs" "geos" "glibmm24" "mozilla-openh264" "hdf5" "libicu" "dyninst" "webkit2gtk3-jsc"
+  "webkit2gtk3-jsc" "libjxl" "libkml" "libmusicbrainz5" "openexr-libs" "openh264" "mesa-libOSMesa" "proj"
+  "qt5-qtwayland" "qt5-qtxmlpatterns" "SDL2_image" "libstdc++" "taglib" "libreoffice-ure" "vtk"
+  "webrtc-audio-processing" "python3"
+)
+
+# failed could not find "libicu67" (removed)
+
+# We need to use eu-readelf to get .gnu-debuglink
+dnf install -y elfutils
+
+# install debug info plugin
+dnf install -y dnf-plugins-core
+
+dnf install -y findutils tree
+for lib in "${libs[@]}"; do
+   printf "dnf debuginfo-install -y ${lib}\n"
+   
+   # Adds debug info to (same path) plus debug in /usr/lib/debug
+   dnf debuginfo-install -y ${lib}
+   dnf install -y ${lib}
+done
---
+++ test_libs.py
@@ -0,0 +1,91 @@
+# Find any .so libraries in a dest and move to a source.
+
+import os
+import sys
+import fnmatch
+import shutil
+import json
+
+import spliced.experiment.manual
+import spliced.utils as utils
+from spliced.logger import logger
+
+
+def recursive_find(base):
+    for root, _, filenames in os.walk(base):
+        for filename in filenames:
+            yield os.path.join(root, filename)
+
+
+def get_prefix(lib):
+    return os.path.basename(lib).split(".", 1)[0]
+
+
+def main(first, second, os_a, os_b, outdir):
+
+    # Create a lookup of prefixes for second libs
+    prefixes = {}
+    found = [x for x in list(recursive_find(second)) if "debug" not in x]
+    print("Found %s libs" % len(found))
+    for lib in found:
+        prefixes[get_prefix(lib)] = lib
+
+    # Count in advance
+    libs = list(recursive_find(first))
+
+    # Match first and second libs on .so
+    # These should already be realpath from find_libs.py
+    for i, lib in enumerate(libs):
+        # Skip debug files
+        if "debug" in lib or "dwz" in lib:
+            continue
+        print("Looking for match to %s: %s of %s" % (lib, i, len(libs)))
+        lib = os.path.abspath(lib)
+        lib_dir = os.path.dirname(lib).replace(first, "").strip("/")
+        prefix = get_prefix(lib)
+        print("Matching prefix %s" % prefix)
+        if prefix in prefixes:
+            second_lib = prefixes[prefix]
+            print("Found match %s for %s" % (second_lib, lib))
+        else:
+            print("Did not find match for %s" % lib)
+            continue
+
+        experiment = "%s-%s-%s" % (prefix, os_a, os_b)
+        outfile = os.path.join(outdir, "%s.json" % experiment)
+        if not os.path.exists(outfile):
+            run_spliced(lib, second_lib, experiment, outfile)
+
+
+def run_spliced(A, B, experiment_name, outfile):
+    """
+    Run a manual experiment
+    """
+    # A general SpackExperiment does a replacement
+    experiment = spliced.experiment.manual.ManualExperiment()
+    experiment.init(package=A, splice=B, experiment=experiment_name)
+
+    # Perform the experiment
+    experiment.run()
+    experiment.predict(None, skip=["spack-test", "smeagle"], predict_type="diff")
+    results = experiment.to_dict()
+    utils.mkdir_p(os.path.dirname(os.path.abspath(outfile)))
+    utils.write_json(results, outfile)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("Expecting a first and second directory of libs as two arguments.")
+    first = os.path.abspath(sys.argv[1])
+    second = os.path.abspath(sys.argv[2])
+    os_a = sys.argv[3]
+    os_b = sys.argv[4]
+    outdir = os.path.abspath(sys.argv[5])
+    print(f"First directory {first}")
+    print(f"Second directory {second}")
+    print(f"OS A {os_a}")
+    print(f"OS B {os_b}")
+    print(f"Output directory {outdir}")
+    os.listdir(first)
+    os.listdir(second)
+    main(first, second, os_a, os_b, outdir)
