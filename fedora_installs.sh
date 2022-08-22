ae60fee725d3f49926d88f4933fc7ec1075a7b8a
Tim Haines <thaines.astro@gmail.com>
2022-08-22 13:30:45 -0500

    Fedora tests- only copy .so files from requested packages
    Not everything in /lib or /lib64 has an associated .debug file. This
    ensures we collect only the .so files we care about for testing.

--- fedora_installs.sh
+++ fedora_installs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+dest_dir=$1
+if test "x$dest_dir" = "x"; then
+   echo "Usage: $0 DIR"
+   exit -1
+fi
+
 declare -a libs=("libadwaita-qt5" "aspell" "boost-log", "clucene-core" "libdap" "samba-libs" "djvulibre-libs"
   "dovecot" "exiv2-libs" "gdal-libs" "geos" "glibmm24" "mozilla-openh264" "hdf5" "libicu" "dyninst" "webkit2gtk3-jsc"
   "webkit2gtk3-jsc" "libjxl" "libkml" "libmusicbrainz5" "openexr-libs" "openh264" "mesa-libOSMesa" "proj"
@@ -22,4 +28,10 @@ for lib in "${libs[@]}"; do
    # Adds debug info to (same path) plus debug in /usr/lib/debug
    dnf debuginfo-install -y ${lib}
    dnf install -y ${lib}
+   
+   # Copy the library's .so files into $dest_dir
+   files=$(rpm -ql $lib | grep -E ".*\.so")
+   for f in $files; do
+      cp -Lu --parents $f $dest_dir/
+   done
 done
