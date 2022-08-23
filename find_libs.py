d3fd630c77e052eac2e1a1698c0c0a9f60599dd0
vsoch <vsoch@users.noreply.github.com>
2022-08-23 11:48:04 -0600

    undo deletions
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ find_libs.py
@@ -0,0 +1,119 @@
+# Find any .so libraries in a dest and move to a source.
+
+import os
+import json
+import sys
+import shutil
+import subprocess
+
+debug_prefix = "/usr/lib/debug"
+
+
+def recursive_find(base):
+    for root, _, filenames in os.walk(base):
+        for filename in filenames:
+            yield os.path.join(root, filename)
+
+
+def add_debug_info(lib, dest_lib):
+    """
+    Get lookup of debug file prefixes.
+    """
+    print("Looking for debuginfo file for %s" % lib)
+    cmd = ["eu-readelf", "--string-dump=.gnu_debuglink", lib]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    if p.returncode != 0:
+        sys.exit(f"Issue asking for debug for {lib}")
+    lines = [
+        x for x in stdout.decode("utf-8", errors="ignore").split("\n") if x.strip()
+    ]
+    debug_file = lines[1]
+    debug_file = debug_file.split(" ")[-1]
+    print("Looking for name %s" % debug_file)
+    if "debug" not in debug_file:
+        print("Cannot find debug file in %s" % debug_file)
+        return
+    debug_path = "/usr/lib/debug" + os.path.dirname(lib) + "/" + debug_file
+    if not os.path.exists(debug_path):
+        print("Debug file %s does not exist" % debug_path)
+        return
+    # Add debug info back
+    cmd = ["eu-unstrip", lib, debug_path, "-o", dest_lib]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    if p.returncode != 0:
+        print(f"Issue adding debug info back {stderr}")
+        return
+    return dest_lib
+
+
+def main(src, dest):
+
+    # Must end in trailing slash
+    if not src.endswith("/"):
+        src = src + "/"
+    if not dest.endswith("/"):
+        dest = dest + "/"
+
+    print("Source: %s" % src)
+    print("Dest: %s" % dest)
+    found_debug = []
+
+    # Find so libs, along with debug
+    for lib in recursive_find(src):
+        if ".so" not in lib:
+            continue
+
+        print(f"PARSING LIBRARY {lib}")
+        original = lib
+        lib = os.path.realpath(lib)
+        basename = os.path.basename(lib)
+
+        # Skip hidden files
+        if basename.startswith("."):
+            continue
+        lib_dir = os.path.dirname(original).replace(src, "")
+        dest_lib = os.path.join(dest, lib_dir, os.path.basename(lib))
+        dirname = os.path.dirname(dest_lib)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+        if os.path.exists(dest_lib):
+            continue
+        # Either we write result to new location with debug
+        try:
+            dest_lib = add_debug_info(lib, dest_lib)
+        except:
+            continue
+        if not dest_lib:
+            continue
+        found_debug.append(dest_lib)
+
+        # or copy the original
+        if not os.path.exists(dest_lib):
+            print("Copying %s to %s" % (lib, dest_lib))
+            shutil.copyfile(lib, dest_lib)
+
+    print("Found %s libraries with debug" % (len(found_debug)))
+
+    # Copy dwz
+    dwz = os.path.join(dest, ".dwz")
+    os.makedirs(dwz)
+    for path in ["/usr/lib/debug/usr/.dwz", "/usr/lib/debug/.dwz"]:
+        if os.path.exists(path):
+            for filename in recursive_find(path):
+                basename = os.path.basename(filename)
+                dest_path = os.path.join(dwz, basename)
+                if not os.path.exists(dest_path):
+                    shutil.copyfile(filename, dest_path)
+    os.listdir(dwz)
+    print(json.dumps(found_debug))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("Expecting a src and dest as two arguments.")
+    src = os.path.abspath(sys.argv[1])
+    dest = os.path.abspath(sys.argv[2])
+    main(src, dest)
