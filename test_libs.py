c8bc3e53c22f2763fe6f329a0948eb3a8e5a152e
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-09-06 20:27:52 -0600

    Update test_libs.py

--- test_libs.py
+++ test_libs.py
@@ -156,7 +156,7 @@ def get_symbols(path):
         return {}
     symbols = [x.strip() for x in utils.read_file(out).split("\n") if x.strip()]
     os.remove(out)
-    return symbols
+    return {"symbols": symbols}
 
 
 def run_symbols_diff(A, B, first, second, experiment_name, outfile):
